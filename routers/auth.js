36675e4d0ba79f96e8ed7b5f948ae5ea1348245a
MohNasr <mohnasr113377@gmail.com>
2023-12-18 18:12:36 +0200

    add sign in feature

--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcryptjs = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
 //sign up
@@ -14,14 +15,6 @@ authRouter.post("/signUp", async (req, res) => {
         .status(400)
         .json({ message: "the user with same email already exists!" });
     }
-    const re =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (!email.match(re)) {
-      return res.status(400).json({ message: "please enter a valid email" });
-    }
-    if (password.length < 6) {
-      return res.status(400).json({ message: "please enter a long password" });
-    }
     const hashedPassword = await bcryptjs.hash(password, 8);
     let user = new User({
       name,
@@ -32,7 +25,28 @@ authRouter.post("/signUp", async (req, res) => {
     user = await user.save();
     res.json(user);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//sign in
+authRouter.post("/signIn", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "the user with this email does not exist!" });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password." });
+    }
+    const token = jwt.sign({ id: user._id }, "key");
+    res.json({ token, ...user._doc });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
